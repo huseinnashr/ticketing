69d7f17daa6a211ab28037f8855943893fb79688
Husein <nvdhunter007@gmail.com>
2020-06-02 00:47:41 +0700

    order index route requrire auth & validation

--- orders/src/routes/index.ts
+++ orders/src/routes/index.ts
@@ -1,9 +1,24 @@
+import mongoose from "mongoose";
 import express, { Request, Response } from "express";
+import { requireAuth, validateRequest } from "@hntickets/common";
+import { body } from "express-validator";
 
 const router = express.Router();
 
-router.get("/api/orders", async (req: Request, res: Response) => {
-  res.send({});
-});
+router.get(
+  "/api/orders",
+  requireAuth,
+  [
+    body("ticketId")
+      .not()
+      .isEmpty()
+      .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+      .withMessage("TicketId must be provided"),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    res.send({});
+  }
+);
 
 export { router as indexOrderRouter };
