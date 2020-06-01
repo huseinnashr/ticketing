b94b95ca02a7662c07e2d791c27b84b7ed61e063
Husein <nvdhunter007@gmail.com>
2020-06-01 20:15:40 +0700

    graceful close nats client on proc exit,viceversa

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -12,6 +12,18 @@ const start = async () => {
 
   try {
     await natsWrapper.connect("ticketing", "sadas", "http://nats-srv:4222");
+    natsWrapper.client.on("close", () => {
+      console.log("NATS connection closed");
+    });
+
+    natsWrapper.client.on("close", () => {
+      console.log("NATS connection closed!");
+      process.exit();
+    });
+
+    process.on("SIGINT", () => natsWrapper.client.close());
+    process.on("SIGTERM", () => natsWrapper.client.close());
+
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
