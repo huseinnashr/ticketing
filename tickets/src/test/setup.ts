a1d66478157b81ae22180c07fa2d348a6a3392d6
Husein <nvdhunter007@gmail.com>
2020-06-01 21:13:03 +0700

    mocks nats connection

--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -10,6 +10,8 @@ declare global {
   }
 }
 
+jest.mock("../nats-wrapper");
+
 let mongo: MongoMemoryServer;
 beforeAll(async () => {
   process.env.JWT_KEY = "asdfasdf";
@@ -24,6 +26,7 @@ beforeAll(async () => {
 });
 
 beforeEach(async () => {
+  jest.clearAllMocks();
   const collections = await mongoose.connection.db.collections();
 
   for (let collection of collections) {
