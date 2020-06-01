fd409ffca29b89e41fbe8ea87639e4596a899196
Husein <nvdhunter007@gmail.com>
2020-06-01 20:31:24 +0700

    create & use ticket updated publisher

--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -7,6 +7,8 @@ import {
   NotAuthorizedError,
 } from "@hntickets/common";
 import { Ticket } from "../models/tickets";
+import { TicketUpdatedPublisher } from "../events/publishers/ticket-updated-publisher";
+import { natsWrapper } from "../nats-wrapper";
 
 const router = express.Router();
 
@@ -34,6 +36,12 @@ router.put(
 
     ticket.set({ title, price });
     await ticket.save();
+    new TicketUpdatedPublisher(natsWrapper.client).publish({
+      id: ticket.id,
+      title: ticket.title,
+      price: ticket.price,
+      userId: ticket.userId,
+    });
 
     res.send(ticket);
   }
