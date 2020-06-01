c529a64da957d1c980cc136126111e789810b313
Husein <nvdhunter007@gmail.com>
2020-06-01 16:06:52 +0700

    type check ticket created publisher

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -1,4 +1,5 @@
 import nats from "node-nats-streaming";
+import { TicketCreatedPublisher } from "./events/ticket-created-publisher";
 
 console.clear();
 
@@ -9,13 +10,6 @@ const stan = nats.connect("ticketing", "abc", {
 stan.on("connect", () => {
   console.log("Publisher connected to NATS");
 
-  const data = JSON.stringify({
-    id: "12113",
-    title: "concert",
-    price: 20,
-  });
-
-  stan.publish("ticket:created", data, () => {
-    console.log("Event published");
-  });
+  const publisher = new TicketCreatedPublisher(stan);
+  publisher.publish({ id: "123", title: "Concert", price: 20 });
 });
