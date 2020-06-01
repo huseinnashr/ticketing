f6bf16e5e66929d1b8c756371519f4bf819dea2f
Husein <nvdhunter007@gmail.com>
2020-06-01 16:12:30 +0700

    promisify publish method

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -7,11 +7,15 @@ const stan = nats.connect("ticketing", "abc", {
   url: "http://localhost:4222",
 });
 
-stan.on("connect", () => {
+stan.on("connect", async () => {
   console.log("Publisher connected to NATS");
 
   const publisher = new TicketCreatedPublisher(stan);
-  publisher.publish({ id: "123", title: "Concert", price: 20 });
+  try {
+    await publisher.publish({ id: "123", title: "Concert", price: 20 });
+  } catch (err) {
+    console.error(err);
+  }
 });
 
 process.on("SIGINT", () => {
