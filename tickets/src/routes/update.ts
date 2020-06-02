dcde7973775292075b1380145ee2de6dff31c511
Husein <nvdhunter007@gmail.com>
2020-06-03 03:24:11 +0700

    implement&test: cant edit reserve ticket

--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -5,6 +5,7 @@ import {
   NotFoundError,
   requireAuth,
   NotAuthorizedError,
+  BadRequestError,
 } from "@hntickets/common";
 import { Ticket } from "../models/tickets";
 import { TicketUpdatedPublisher } from "../events/publishers/ticket-updated-publisher";
@@ -28,6 +29,10 @@ router.put(
 
     if (!ticket) {
       throw new NotFoundError();
+    }
+
+    if (ticket.orderId) {
+      throw new BadRequestError("Cannot edit a reserved ticket");
     }
 
     if (ticket.userId != req.currentUser!.id) {
