32fdf6dd9b578c66446c86b90f2d09431716dc6b
Husein <nvdhunter007@gmail.com>
2020-06-02 12:23:24 +0700

    publish order created and cancelled event

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -6,6 +6,8 @@ import {
   NotAuthorizedError,
 } from "@hntickets/common";
 import { Order } from "../models/order";
+import { OrderCancelledPublisher } from "../events/publisher/order-cancelled-publisher";
+import { natsWrapper } from "../nats-wrapper";
 
 const router = express.Router();
 
@@ -15,7 +17,7 @@ router.delete(
   async (req: Request, res: Response) => {
     const { orderId } = req.params;
 
-    const order = await Order.findById(orderId);
+    const order = await Order.findById(orderId).populate("ticket");
 
     if (!order) {
       throw new NotFoundError();
@@ -25,6 +27,13 @@ router.delete(
     }
     order.status = OrderStatus.Cancelled;
     order.save();
+
+    new OrderCancelledPublisher(natsWrapper.client).publish({
+      id: order.id,
+      ticket: {
+        id: order.ticket.id,
+      },
+    });
 
     res.status(204).send(order);
   }
