c58304167690f181e7b9fdf057c75f2435dadf1a
Husein <nvdhunter007@gmail.com>
2020-06-03 16:21:38 +0700

    order created listener class for expiration srv

---
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -0,0 +1,10 @@
+import { Listener, OrderCreatedEvent, Subjects } from "@hntickets/common";
+import { Message } from "node-nats-streaming";
+import { queueGroupName } from "./queue-group-name";
+
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+  subject: Subjects.OrderCreated = Subjects.OrderCreated;
+  queueGroupName = queueGroupName;
+
+  onMessage(data: OrderCreatedEvent["data"], msg: Message) {}
+}
